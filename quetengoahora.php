3fb6ef29d60ba30ca3d8986c33fb9603214bd804
Juan Miguel <redxiii_cloud_vincent@hotmail.com>
2022-09-19 10:59:34 +0100

    Create quetengoahora.php

---
+++ quetengoahora.php
@@ -0,0 +1,178 @@
+<html>
+
+    <?php
+    //$time=time();
+    $array=array(
+        "Lunes"=>array(
+            1=>array(
+                "Empresa e Iniciativa Emprendedora (EMR)"
+                "Maria del Sol Garcia Tarajano"
+                "Aula 201"
+            );
+            2=>array(
+                "Desarrollo Web en Entorno de Servidor (DSW)"
+                "Sergio Ramos Suarez"
+                "Aula 201"
+            );
+            3=>array(
+                "Desarrollo Web en Entorno de Servidor (DSW)"
+                "Sergio Ramos Suárez"
+                "Aula 201"
+            );
+            4=>array(
+                "Desarrollo Web en Entorno de Cliente (DEW)"
+                "Maria del Carmen Rodríguez Suárez"
+                "Aula 201"
+            );
+            5=>array(
+                "Desarrollo Web en Entorno de Cliente (DEW)"
+                "Maria del Carmen Rodríguez Suárez"
+                "Aula 201"
+            );
+            6=>array(
+                "Desarrollo Web en Entorno de Cliente (DEW)"
+                "Maria del Carmen Rodríguez Suárez"
+                "Aula 201"
+            );
+        );
+        "Martes"=>array(
+            1=>array(
+                "Despliegue de Aplicaciones Web (DPL)"
+                "Maria Antonia Montesdeoca Viera"
+                "Aula 201"
+            );
+            2=>array(
+                "Despliegue de Aplicaciones Web (DPL)"
+                "Maria Antonia Montesdeoca Viera"
+                "Aula 201"
+            );
+            3=>array(
+                "Desarrollo Web en Entorno de Servidor (DSW)"
+                "Sergio Ramos Suárez"
+                "Aula 201"
+            );
+            4=>array(
+                "Desarrollo Web en Entorno de Servidor (DSW)"
+                "Sergio Ramos Suárez"
+                "Aula 201"
+            );
+            5=>array(
+                "Diseño de Interfacez Web (DOR)"
+                "Ermis Papakonstantinou Baez"
+                "Aula 201"
+            );
+            6=>array(
+                "Diseño de Interfacez Web (DOR)"
+                "Ermis Papakonstantinou Baez"
+                "Aula 201"
+            );
+        );
+        "Miércoles"=>array(
+            1=>array(
+                "Desarrollo Web en Entorno de Cliente (DEW)"
+                "Maria del Carmen Rodríguez Suárez"
+                "Aula 201"
+            );
+            2=>array(
+                "Desarrollo Web en Entorno de Cliente (DEW)"
+                "Maria del Carmen Rodríguez Suárez"
+                "Aula 201"
+            );
+            3=>array(
+                "Desarrollo Web en Entorno de Servidor (DSW)"
+                "Sergio Ramos Suárez"
+                "Aula 201"
+            );
+            4=>array(
+                "Desarrollo Web en Entorno de Servidor (DSW)"
+                "Sergio Ramos Suárez"
+                "Aula 201"
+            );
+            5=>array(
+                "Diseño de Interfacez Web (DOR)"
+                "Ermis Papakonstantinou Baez"
+                "Aula 201"
+            );
+            6=>array(
+                "Diseño de Interfacez Web (DOR)"
+                "Ermis Papakonstantinou Baez"
+                "Aula 201"
+            );
+        );
+        "Jueves"=>array(
+            1=>array(
+                "Despliegue de Aplicaciones Web (DPL)"
+                "Maria Antonia Montesdeoca Viera"
+                "Aula 201"
+            );
+            2=>array(
+                "Despliegue de Aplicaciones Web (DPL)"
+                "Maria Antonia Montesdeoca Viera"
+                "Aula 201"
+            );
+            3=>array(
+                "Despliegue de Aplicaciones Web (DPL)"
+                "Maria Antonia Montesdeoca Viera"
+                "Aula 201"
+            );
+            4=>array(
+                "Desarrollo Web en Entorno de Cliente (DEW)"
+                "Maria del Carmen Rodríguez Suárez"
+                "Aula 201"
+            );
+            5=>array(
+                "Desarrollo Web en Entorno de Cliente (DEW)"
+                "Maria del Carmen Rodríguez Suárez"
+                "Aula 201"
+            );
+            6=>array(
+                "Empresa e Iniciativa Emprendedora (EMR)"
+                "Maria del Sol Garcia Tarajano"
+                "Aula 201"
+            );
+        );
+        "Viernes"=array(
+            1=>array(
+                "Diseño de Interfacez Web (DOR)"
+                "Ermis Papakonstantinou Baez"
+                "Aula 201"
+            );
+            2=>array(
+                "Diseño de Interfacez Web (DOR)"
+                "Ermis Papakonstantinou Baez"
+                "Aula 201"
+            );
+            3=>array(
+                "Despliegue de Aplicaciones Web (DPL)"
+                "Maria Antonia Montesdeoca Viera"
+                "Aula 201"
+            );
+            4=>array(
+                "Empresa e Iniciativa Emprendedora (EMR)"
+                "Maria del Sol Garcia Tarajano"
+                "Aula 201"
+            );
+            5=>array(
+                "Desarrollo Web en Entorno de Servidor (DSW)"
+                "Sergio Ramos Suárez"
+                "Aula 201"
+            );
+            6=>array(
+                "Desarrollo Web en Entorno de Servidor (DSW)"
+                "Sergio Ramos Suárez"
+                "Aula 201"
+            );
+        );
+    );
+    />
+
+
+
+
+
+
+
+
+
+
+</html>
